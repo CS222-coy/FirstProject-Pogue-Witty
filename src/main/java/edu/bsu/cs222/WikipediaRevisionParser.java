808316eb137857446c34317814fd8f99158f7d76
Jennifer Coy <jennifer.coy@bsu.edu>
2022-09-22 15:46:36 -0400

    Initial commit

---
+++ src/main/java/edu/bsu/cs222/WikipediaRevisionParser.java
@@ -0,0 +1,21 @@
+package edu.bsu.cs222;
+
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class WikipediaRevisionParser {
+    public String parse(InputStream testDataStream) throws IOException {
+        JSONArray result = JsonPath.read(testDataStream, "$..timestamp");
+        return result.get(0).toString();
+    }
+
+    public boolean doesPageExist(int value) {
+        return true;
+    }
+}
+
+
+
